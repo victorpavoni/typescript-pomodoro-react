7db07730e12999a049125570152599adc4cf22c0
victorpavoni <victor.gattaz.pavoni@gmail.com>
2023-07-24 17:26:44 -0300

    Initial commit 🚀

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { PomodoroTimer } from "./components/pomodoro-timer";
+
+function App() {
+  return (
+    <div className="container">
+      <PomodoroTimer
+        pomodoroTime={10}
+        shortRestTime={2}
+        longRestTime={5}
+        cycles={4}
+      />
+    </div>
+  );
+}
+
+export default App;
